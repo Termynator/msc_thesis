927930f134b0840816e4fd75f2e9578780ead24c
Termynator <zekeortiz00@gmail.com>
2020-08-18 20:33:55 +0100

    exp 2 params adj

--- matlab/simtb/examples/comp_A_params_large_diff_02.m
+++ matlab/simtb/examples/comp_A_params_large_diff_02.m
@@ -53,8 +53,7 @@ TR = 2;   % repetition time
 % Choose the sources. To launch a stand-alone GUI:
 % >> simtb_pickSM 
 
-SM_source_ID = [       3  4  5  6  7  8  9     ...
-                11 12    14 15 16 17 18 19 20  ...
+SM_source_ID = [11 12    14 15 16 17 18 19 20  ...
                 21 22 23 24 25 26 27 28 29 30]; % all but (1, 2, 10, 13)
 
 nC = length(SM_source_ID);  % number of components            
